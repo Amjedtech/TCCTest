3313115cbb629adb9880005937e89043367e6e19
Amjedtech <63866737+Amjedtech@users.noreply.github.com>
2020-11-17 06:42:42 +0200

    added sample code

--- وظيفة التقانات3/WebForm1.aspx.designer.cs
+++ وظيفة التقانات3/WebForm1.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace وظيفة_التقانات3 {
         protected global::System.Web.UI.WebControls.TextBox txtFrom;
         
         /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
         /// txtTo control.
         /// </summary>
         /// <remarks>
